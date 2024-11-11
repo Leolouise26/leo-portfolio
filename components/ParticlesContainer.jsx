1f2241d8c3f669e9dfbaf6398c5c21d07f445175
Leocchi <128828635+Leolouise26@users.noreply.github.com>
2024-11-11 22:49:06 +0800

    react-tsparticles

--- components/ParticlesContainer.jsx
+++ components/ParticlesContainer.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useCallback } from "react";
-import {Particles} from "react-tsparticles"
+import {Particles} from "react-tsparticles"; 
 import {loadFull} from "tsparticles";
 
 const ParticlesContainer = () => {
